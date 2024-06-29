a1a474b63d4ddf4aa37b83a4a5b4b08905d15369
Anik Kumar Nandi <aniknandi.it@gmail.com>
2024-06-30 00:50:21 +0600

    added skills

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const activeClass = 'bg-clip-text font-semibold		 text-transparent bg-gradient-to-r from-green-300 0 to-indigo-500 font-black';
 
+$($("ul li a")[0]).addClass(activeClass)
+
 $("ul li a").click(function(){
     const navLinks = $("ul li a");
     for (let index = 0; index < navLinks.length; index++) {
@@ -7,4 +9,19 @@ $("ul li a").click(function(){
         
     }
     $(this).addClass(activeClass)
+})
+
+$("#nav-skills").click(function(){
+    $(".section-wrapper").addClass("hidden")
+    $("#skills").removeClass("hidden")
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+
+})
+
+$("#nav-home").click(function(){
+    $(".section-wrapper").addClass("hidden")
+    $("#home").removeClass("hidden")
+
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+
 })
